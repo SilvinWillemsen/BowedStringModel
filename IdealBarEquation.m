d1db11cf79969a4cc7c7082c3eeb41711cefb5de
SilvinWillemsen <silvinwillemsen@gmail.com>
2018-08-27 15:01:54 +0200

    Implemented energy in Ideal Bar model. Added images of energy for the 1d wave equation.

--- IdealBarEquation.m
+++ IdealBarEquation.m
@@ -2,71 +2,84 @@ clear all;
 close all;
 clc;
 
-fs = 44100;     % Sampling rate
-E = 2E11;       % Young's Modulus
-L = 0.64;       % String length
+fs = 8000;      % Sampling rate
+k = 1 / fs;     % Time step
+L = 1;          % String length
 rho = 7850;     % Density of steel [kg/m^3]
-
 r = 0.001;      % String radius
-inertia = pi / 4 * r^4;         % Moment of inertia
-kappa = sqrt((E * inertia) / rho);  % Stiffness
+A = pi * r^2;   % Cross-sectional area
+E = 2E11;       % Young's Modulus
+inertia = pi / 4 * r^4; % Moment of inertia
+kappa = sqrt((E * inertia) / (rho * A * L^4));  % Stiffness
 
-k = 1/fs;
-h = sqrt(2 * kappa * k) * 1.2;
-muSq = (k * kappa / h^2)^2;
+h = sqrt(2 * kappa * k); % Grid spacing
+muSq = (k * kappa / h^2)^2; % Courant number squared
 
-P = 1/3;
+P = 1/2; % plucking position
 
-n = floor(L / h) + 1;
+n = floor(L / h) + 1; % Number of grid-points
 
-cosWidth = round(n / 30);
+%% Raised cosine input
+cosWidth = round(n / 20);
 raisedCos = 0.5 * (cos(pi:(2*pi)/cosWidth:3*pi) + 1);
 PIdx = floor (P * n);
+
+%% Initialise state vectors
 u = zeros(n, 1);
 u(floor(n * P - cosWidth / 2 : ceil(n * P + cosWidth / 2))) = raisedCos;
 uPrev = u;
-plot (u);
 uNext = zeros(n,1);
-% Dxxxx = (sparse(3:n, 1:n-2, ones(1, n-2), n, n) + ...
-%         sparse(2:n, 1:n-1, -4 * ones(1, n-1), n, n) + ...
-%         sparse(1:n, 1:n, 6 * ones(1, n), n, n) + ...
-%         sparse(1:n-1, 2:n, -4 * ones(1, n-1), n, n) + ...
-%         sparse(1:n-2, 3:n, ones(1, n-2), n, n));
+
+%% Matrix representation
+
 Dxx = (sparse(2:n, 1:n-1, ones(1, n-1), n, n) + ...
     sparse(1:n, 1:n, -2 * ones(1, n), n, n) + ...
     sparse(1:n-1, 2:n, ones(1, n-1), n, n));
-% Dxx(:,1:2) = 0;
-% Dxx(:,end-1:end) = 0;
-% Dxx(1:2,:) = 0;
-% Dxx(end-1:end,:) = 0;
 Dxxxx = Dxx * Dxx;
 
 I = sparse(1:n, 1:n, ones(1, n), n, n);
 B = 2 * I - muSq * Dxxxx;
 Buse = B(3:n-2, 3:n-2);
-lengthSound = fs * 10;
+lengthSound = fs / 10;
 out = zeros(lengthSound, 1);
+
+drawBar = false;
+range = 3 : n-2;
+
+kinEnergy = zeros(lengthSound,1);
+potEnergy = zeros(lengthSound,1);
+
 for t = 1 : lengthSound
-    uNext(3:n-2) = B(3:n-2,3:n-2) * u(3:n-2) - uPrev(3:n-2);
+    uNext(range) = Buse * u(range) - uPrev(range);
     out(t) = uNext(length(uNext) - PIdx);
+    kinEnergy(t) = 1 / 2 * sum (h * ((1 / k * (u(range) - uPrev(range))).^2));
+    potEnergy(t) = kappa^2 / 2 * sum (1 / h^3 * ...
+        (u(range + 1) - 2 * u(range) + u(range - 1)) .* ...
+        (uPrev(range + 1) - 2 * uPrev(range) + uPrev(range - 1)));
     uPrev = u;
     u = uNext;
-    if mod(t,100) == 1
+    
+    if mod(t,2) == 1 && drawBar
         plot(uNext);
         ylim([-1 1]);
         drawnow;
     end
 end
+plot(kinEnergy);
+hold on; plot(potEnergy);
+totEnergy = kinEnergy + potEnergy;
+plot(totEnergy);
 plot(out);
 
-for t = 1 : fs * 3
-    for x = 3 : length(u) - 2
-        uNext(x) = (2 - 6 * muSq) * u(x) + ...
-                    4 * muSq * (u(x+1) + u(x-1)) - ...
-                    muSq * (u(x-2) + u(x+2)) - uPrev(x);
-    end
-    out(t) = uNext(PIdx);
-    uPrev = u;
-    u = uNext;
-end
-plot(out);
+%% For-loop representation
+% for t = 1 : fs * 3
+%     for x = 3 : length(u) - 2
+%         uNext(x) = (2 - 6 * muSq) * u(x) + ...
+%                     4 * muSq * (u(x+1) + u(x-1)) - ...
+%                     muSq * (u(x-2) + u(x+2)) - uPrev(x);
+%     end
+%     out(t) = uNext(PIdx);
+%     uPrev = u;
+%     u = uNext;
+% end
+% plot(out);
--- WaveEquation1D.m
+++ WaveEquation1D.m
@@ -2,11 +2,11 @@ clear all;
 close all;
 clc;
 
-fs = 8000;     % Sampling rate
+fs = 8000;      % Sampling rate
 k = 1 / fs;     % Time step
 L = 0.40;       % String length
 rho = 7850;     % Density of steel [kg/m^3]
-c = 100;         % sqrt(T / rho);% Wave speed
+c = 100;        % Wave speed
 h = c * k;      % Grid spacing
 
 lambdaSq = (c * k / h)^2; % Courant number squared
@@ -19,39 +19,72 @@ n = floor(L / h) + 1; % Number of grid-points
 cosWidth = round(n / 10);
 raisedCos = 0.5 * (cos(pi:(2*pi)/cosWidth:3*pi) + 1);
 PIdx = floor (P * n);
-u = zeros(n, 1);
+
+%% Initialise state vectors
+u = zeros(n + 1, 1);
 u(floor(n * P - cosWidth / 2 : ceil(n * P + cosWidth / 2))) = raisedCos;
 uPrev = u;
 
+%% Boundary Conditions
+bound = "Dir";
+
 %% Matrix Representation
-uNext = zeros(n, 1);
-Dxx = (sparse(2:n, 1:n-1, ones(1, n-1), n, n) + ...
-    sparse(1:n, 1:n, -2 * ones(1, n), n, n) + ...
-    sparse(1:n-1, 2:n, ones(1, n-1), n, n));
-I = sparse(1:n, 1:n, ones(1, n), n, n);
+uNext = zeros(n + 1, 1);
+
+if strcmp(bound, "Dir")
+    n = n - 2;
+    Dxx = (sparse(2:n, 1:n-1, ones(1, n-1), n, n) + ...
+        sparse(1:n, 1:n, -2 * ones(1, n), n, n) + ...
+        sparse(1:n-1, 2:n, ones(1, n-1), n, n));
+    I = sparse(1:n, 1:n, ones(1, n), n, n);
+    n = n + 2;
+    range = 2 : n - 1;  
+else
+    if strcmp(bound, "Neu")
+        Dxx = (sparse(2:n, 1:n-1, [ones(1, n-2) 2], n, n) + ...
+            sparse(1:n, 1:n, -2 * ones(1, n), n, n) + ...
+            sparse(1:n-1, 2:n, [2 ones(1, n-2)], n, n));
+        I = sparse(1:n, 1:n, ones(1, n), n, n);
+        range = 1 : n; 
+    end
+end
+
 B = 2 * I + lambdaSq * Dxx;
 
 lengthSound = fs / 10;
 out = zeros(lengthSound, 1);
 kinEnergy = zeros(lengthSound,1);
 potEnergy = zeros(lengthSound,1);
+plotString = false;
 
-
-range = 2 : n - 1;
 for t = 1 : lengthSound
-    uNext(range) = B(range,range) * u(range) - uPrev(range);
-    kinEnergy(t) = sum (1 / 2 * h * ((1 / k * (u(range) - uPrev(range))).^2));
-    potEnergy(t) = sum (c^2 / 2 * 1 / h * ...
+    uNext(range) = B * u(range) - uPrev(range);
+    kinEnergy(t) = 1 / 2 * sum (h * ((1 / k * (u(range) - uPrev(range))).^2));
+    potEnergy(t) = c^2 / 2 * sum (1 / h * ...
         (u(range + 1) - u(range)) .* (uPrev(range + 1) - uPrev(range)));
     out(t) = uNext(PIdx);
-
+    
+    if plotString
+        plot(uNext);
+        ylim([-1 1]);
+        drawnow;
+    end
+    
     uPrev = u;
     u = uNext;
 end
 totEnergy = kinEnergy + potEnergy;
-plot(totEnergy);
- 
-% %% For-loop representation
+clf;
+plot(kinEnergy, 'LineWidth', 1); 
+hold on; 
+plot(potEnergy, 'LineWidth', 1); 
+plot(totEnergy, 'LineWidth', 1);
+legend('Kinetic', 'Potential', 'Total');
+xlabel('t (in samples)');
+ylabel('Energy');
+set(gca, 'FontSize', 15, 'LineWidth', 1);
+
+%% For-loop representation
 % potEnergy = zeros(lengthSound, 1);
 % kinEnergy = zeros(lengthSound, 1);
 % 
